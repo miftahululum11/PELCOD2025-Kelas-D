2196b24758ae02604deacf5e0719691039b6c438
miftahululum11 <ulumkmpx@gmail.com>
2025-09-19 18:48:20 +0700

    250441100064_MiftahulUlum_Tugas-1

---
+++ soal2.py
@@ -0,0 +1,4 @@
+pensil = 2000
+buku = 5000
+total = 4 * pensil + 2 * buku
+print(total)
---
+++ tugas1.py
@@ -0,0 +1,22 @@
+nama = "miftahul ulum allamal maftuh"
+umur = 19
+tinggi = 170 
+angka_favorit = 12
+
+print("hai nama saya", nama ,"saya berumur", umur ,"tinggi saya", tinggi ,"saya suka angka", angka_favorit)
+
+print('='*50)
+
+pensil = 2000
+buku = 5000
+total = 4 * pensil + 2 * buku
+print(total)
+
+print('='*50)
+
+if angka_favorit %2 == 0:
+    print("Angka genap")
+else:
+    print("Angka Ganjil")
+
+print('='*50)
